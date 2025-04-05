50a4439ff35f3fb7f5367adabf06a4f4e844cc3e
Charles Sherwood <kulan876321@yahoo.com>
2025-04-05 18:34:57 -0400

    Formatting

--- Chapter5Exercise20.cpp
+++ Chapter5Exercise20.cpp
@@ -6,144 +6,179 @@ number and have the user guess it correctly and save
 the results of the guess
 */
 #include <ctime>
-#include<cstdlib>
-#include<string>
+#include <cstdlib>
+#include <string>
 #include <iostream>
-#include<fstream>
+#include <fstream>
 using namespace std;
 
+// Prototypes
 int Random();
-bool repeat();
 void Welcome();
 void SaveScore(const string& name, int tries);
 void ReadScores();
 void Menu();
+void DeleteScores();
+void PlayGame();
+
 
 
 int main()
 {
-	srand(time(0));
-	int Guess;
-	string Name;
-	Welcome();
-	cout << "Please Enter Your Name: ";
-	getline(cin, Name);
-		int Rand = Random();
-		int tries = 0;
-		do
-		{
-			cout << "Please Enter Your Guess:";
-			cin >> Guess;
-			tries++;
-			if (Guess > Rand)
-				cout << "**Guess Too High**\n";
-			else if (Guess < Rand)
-				cout << "**Guess Too Low**\n";
-			else
-				cout << "!!Correct!!\n";
-		} while (Guess != Rand);
-		SaveScore(Name, tries);
-		Menu();
-}
- 
-void Menu()
-{
-	int Choice;
-	do
-	{
-		cout << "1-Display Scores\n";
-		cout << "2-Play A New Session\n";
-		cout << "3-Delete Old Sessions\n";
-		cout << "4-Exit The Program\n";
-		cin >> Choice;
-		switch (Choice)
-		{
-		case 1:
-		{
-			ReadScores();
-		}
-		break;
-		case 2:
-		{
-			main();
-		}
-		break;
-		case 3:
-		{
-
-
-		}
-		break;
-		case 4:
-		{
-
-
-		}
-		break;
-		}
-	} while (Choice != 4);
-}
-
-
-int Random()
-{
-	return (rand() % 100 + 1);
+    srand(time(0));
+    Welcome();
+    Menu();
+    return 0;
 }
 
 
 
-bool repeat()
-{
-	char repeat;
-	cin >> repeat;
-	return (repeat == 'Y' || repeat == 'y');
-}
 
-
+// Show welcome message
 void Welcome()
 {
-	cout << "-----------------------------------------\n";
-	cout << "!!!Welcome To The Number Guessing Game!!!\n";
-	cout << "-----------------------------------------\n";
-	cout << "This Program Will Generate A Random Number From 1-100 For You To Guess\n";
+    cout << "-----------------------------------------\n";
+    cout << "!!!Welcome To The Number Guessing Game!!!\n";
+    cout << "-----------------------------------------\n";
+    cout << "This Program Will Generate A Random Number From 1-100 For You To Guess\n";
 }
 
 
 
+// Menu system with switch statements
+void Menu()
+{
+    int Choice;
+    do
+    {
+        cout << "\n=========== MENU ===========\n";
+        cout << "1 - Display Scores\n";
+        cout << "2 - Play A New Session\n";
+        cout << "3 - Delete Old Sessions\n";
+        cout << "4 - Exit The Program\n";
+        cout << "Enter your choice: ";
+        cin >> Choice;
+
+        switch (Choice)
+        {
+        case 1:
+            ReadScores();
+            break;
+
+        case 2:
+            PlayGame();
+            break;
+
+        case 3:
+            DeleteScores();
+            break;
+
+        case 4:
+            cout << "Exiting program. Goodbye!\n";
+            break;
+
+        default:
+            cout << "Invalid option. Please try again.\n";
+        }
+
+    } while (Choice != 4);
+}
+
+
+
+// Has The Guessing Game inside
+void PlayGame()
+{
+    string Name;
+    int Guess;
+    int tries = 0;
+    int Rand = Random();
+
+    cout << "Please Enter Your Name: ";
+    cin.ignore(); // clear leftover newline
+    getline(cin, Name);
+
+    do
+    {
+        cout << "Please Enter Your Guess: ";
+        cin >> Guess;
+        tries++;
+
+        if (Guess > Rand)
+            cout << "**Guess Too High**\n";
+        else if (Guess < Rand)
+            cout << "**Guess Too Low**\n";
+        else
+            cout << "!!Correct!!\n";
+
+    } while (Guess != Rand);
+
+    SaveScore(Name, tries);
+}
+
+
+
+
+// Generate random number
+int Random()
+{
+    return (rand() % 100 + 1);
+}
+
+
+
+// Save name and tries to file
 void SaveScore(const string& name, int tries)
 {
-	ofstream file("scores.txt", ios::app); // append mode
-	if (file.is_open())
-	{
-		file << name << " - " << tries << " tries\n";
-		file.close();
-		cout << "**Score saved to scores.txt!**\n";
-	}
-	else
-	{
-		cerr << "Error: Unable to open scores.txt for writing.\n";
-	}
+    ofstream file("scores.txt", ios::app);
+    if (file.is_open())
+    {
+        file << name << " - " << tries << " tries\n";
+        file.close();
+        cout << "**Score saved to scores.txt!**\n";
+    }
+    else
+    {
+        cerr << "Error: Unable to open scores.txt for writing.\n";
+    }
 }
 
 
 
+
+// Read and print all saved scores
 void ReadScores()
 {
-	ifstream file("scores.txt");
+    ifstream file("scores.txt");
 
-	if (!file.is_open())
-	{
-		cerr << "Error: Unable to open scores.txt for reading.\n";
-		return;
-	}
+    if (!file.is_open())
+    {
+        cerr << "Error: Unable to open scores.txt for reading.\n";
+        return;
+    }
 
-	cout << "\n===== All Saved Scores =====\n";
-	string line;
-	while (getline(file, line))
-	{
-		cout << line << "\n";
-	}
-	cout << "============================\n";
+    cout << "\n===== All Saved Scores =====\n";
+    string line;
+    while (getline(file, line))
+    {
+        cout << line << "\n";
+    }
+    cout << "============================\n";
 
-	file.close();
+    file.close();
+}
+
+
+
+// Delete the scores file
+void DeleteScores()
+{
+    if (remove("scores.txt") == 0)
+    {
+        cout << "**All scores have been deleted!**\n";
+    }
+    else
+    {
+        cerr << "Error: Could not delete scores.txt or file does not exist.\n";
+    }
 }
